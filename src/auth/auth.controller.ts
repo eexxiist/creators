a73ee020471e38ec71b3c3f9f1d0961b1029689b
eexxiist <mego_dilya@mail.ru>
2026-06-10 22:58:54 +0300

    feat: add JWT authentication and Swagger documentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,17 @@
 import { AuthService } from './auth.service';
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  BadRequestException,
+  Headers,
+} from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
   @Get('users')
   async getUsers() {
     return this.authService.getUsers();
@@ -11,6 +19,25 @@ export class AuthController {
 
   @Post('registration')
   async registration(@Body() data: any) {
-    return this.authService.registration(data)
+    return this.authService.registration(data);
+  }
+
+  @Post('login')
+  async login(@Body() data: any) {
+    return this.authService.login(data);
+  }
+
+  @Get('profile')
+  async profile(@Headers() headers: Record<string, string>) {
+    const authorization = headers.authorization;
+
+    if (!authorization) {
+      throw new BadRequestException('Токен не найден');
+    }
+
+    const token = authorization.split(' ')[1];
+    console.log('authorization:', authorization);
+    console.log('token:', token);
+    return this.authService.profile(token);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { generateJwt } from './utils/generateJwt';
+import { verifyToken } from './utils/verifyToken';
 
 @Injectable()
 export class AuthService {
@@ -38,5 +39,47 @@ export class AuthService {
         role: user.role,
       },
     };
+  }
+
+  async login(data: { email: string; password: string }) {
+    const { email, password } = data;
+
+    if (!email || !password) {
+      throw new BadRequestException('Заполните все поля');
+    }
+
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      throw new BadRequestException('Неверный логин или пароль');
+    }
+
+    const passwordInput = await bcrypt.compare(password, user.passwordHash);
+    if (!passwordInput) {
+      throw new BadRequestException('Неверный логин или пароль');
+    }
+
+    const token = generateJwt(user.id, user.role);
+
+    return {
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    };
+  }
+
+  async profile(token: string) {
+    const userId = verifyToken(token).id;
+
+    const user = await this.prisma.user.findUnique({ where: { id: userId } });
+
+    if (!user) {
+      throw new BadRequestException('Вы не зарегистрированы');
+    }
+
+    return user;
   }
 }
