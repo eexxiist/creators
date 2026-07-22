af12d75ab25df6600a5c8abc1cf0dc30556b020d
eexxiist <mego_dilya@mail.ru>
2026-07-22 23:32:31 +0300

    feat: implement follow statistics

--- server/server-nest/src/follow/follow.controller.ts
+++ server/server-nest/src/follow/follow.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { FollowService } from './follow.service';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { type AuthRequest } from 'src/auth/types/auth-request';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('Follow')
 @Controller('follow')
-export class FollowController {}
+export class FollowController {
+  constructor(private readonly followService: FollowService) {}
+
+  @UseGuards(AuthGuard)
+  @Post(':creatorId')
+  async createFollow(
+    @Param('creatorId') creatorId: string,
+    @Req() request: AuthRequest,
+  ) {
+    const userId = request.user.id;
+
+    return this.followService.createFollow(userId, creatorId);
+  }
+
+  @UseGuards(AuthGuard)
+  @Delete(':creatorId')
+  async deleteFollow(
+    @Param('creatorId') creatorId: string,
+    @Req() request: AuthRequest,
+  ) {
+    const userId = request.user.id;
+    return this.followService.deleteFollow(userId, creatorId);
+  }
+
+  @Get(':userId')
+  async getFollowCounts(@Param('userId') userId: string) {
+    return this.followService.getFollowCounts(userId);
+  }
+}
