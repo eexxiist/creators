b53edf493025a487e642cd48f0b580b985b88bd2
eexxiist <mego_dilya@mail.ru>
2026-08-08 14:10:06 +0300

    Refactor project structure

--- client/client/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>Chefs Nest</h1>
-    </main>
-  );
-}
--- client/client/src/hooks/useLogin.ts
+++
@@ -1,6 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { AuthService } from "@/services/auth.service";
-
-export const useLogin = () => {
-    return useMutation({mutationFn: AuthService.login})
-}
